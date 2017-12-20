c48e0ffe41787bfe3c1ac9f83ec523b26102c20e
Erin Bennett <erindb@stanford.edu>
2017-12-20 00:21:05 -0800

    improve language support

--- SERVE.sh
+++ SERVE.sh
@@ -2,21 +2,24 @@
 
 # SERVE.sh - A script to run a corenlp server
 
-if [ "$1" == "" ] || [[ "$1" =~ ^(english|spanish|chinese)$ ]]; then
+if [ "$1" == "" ] || [[ "$1" =~ ^(en|sp|ch)$ ]]; then
 	if [ "$1" == "" ]; then
-		language="english"
+		language="en"
 	else
 		language=$1
 	fi
 	echo "running corenlp server for "$language
 
 	case $language in
-		spanish )
-			properties_tag="-props StanfordCoreNLP-spanish.properties -serverProperties StanfordCoreNLP-spanish.properties" ;;
-		chinese )
-			properties_tag="-props StanfordCoreNLP-chinese.properties -serverProperties StanfordCoreNLP-chinese.properties" ;;
-		english )
-			properties_tag="" ;;
+		sp )
+			properties_tag="-props StanfordCoreNLP-spanish.properties -serverProperties StanfordCoreNLP-spanish.properties"
+			port=12347 ;;
+		ch )
+			properties_tag="-props StanfordCoreNLP-chinese.properties -serverProperties StanfordCoreNLP-chinese.properties"
+			port=12346 ;;
+		en )
+			properties_tag=""
+			port=12345 ;;
 	esac
 
 	cd corenlp
@@ -27,9 +30,13 @@ if [ "$1" == "" ] || [[ "$1" =~ ^(english|spanish|chinese)$ ]]; then
 		silent_tag=""
 	fi
 
-	java -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer \
-		$properties_tag \
-		-port 12345 -timeout 15000 -quiet -preload tokenize,ssplit,pos,depparse $silent_tag
+	server_params=$properties_tag' -port '$port' -timeout 15000 -quiet -preload tokenize,ssplit,pos,depparse '$silent_tag
+	echo $server_params
+	# pwd
+	# echo $server_command
+	# $server_command
+	# java -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer $properties_tag -port $port -timeout 15000 -quiet -preload tokenize,ssplit,pos,depparse $silent_tag
+	java -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer $server_params
 
 	cd ..
 else
