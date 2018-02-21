75fdc5ad407727b91573a23c20b35c7ceb7fb2dc
Erin <erindb@stanford.edu>
2018-02-20 19:44:03 -0800

    rearrange

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import BaseHTTPServer, SimpleHTTPServer
 import ssl
 import requests
+from nlp import process_parsed_data
+import os
+
+SERVER_ADDRESS = (HOST, PORT) = 'rxdhawkins.me', 8882
 
 class S(BaseHTTPServer.BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -19,9 +23,7 @@ class S(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        import requests
-        from nlp import process_parsed_data
-        url = "http://localhost:12401?properties={annotators:'tokenize,ssplit,pos,depparse'}"
+        url = "http://localhost:12345?properties={annotators:'tokenize,ssplit,pos,depparse,lemma,coref'}"
         data = post_data
         print(data)
         parse = requests.post(url, data=data).text
@@ -30,7 +32,7 @@ class S(BaseHTTPServer.BaseHTTPRequestHandler):
         self.wfile.write(output)
         #self.wfile.write("<html><body><h1>POST!</h1></body></html>")
 
-httpd = BaseHTTPServer.HTTPServer(('rxdhawkins.me', 54321), S)
+httpd = BaseHTTPServer.HTTPServer(('rxdhawkins.me', 8882), S)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='/etc/apache2/ssl/rxdhawkins.me.pem', server_side=True)
 print "serving..."
 httpd.serve_forever()
